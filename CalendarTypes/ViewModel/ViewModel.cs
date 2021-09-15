58ef1c63aabb607aef64ded4ca3fade8d1e17072
Naveenklumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2021-09-15 12:00:35 +0530

    Added scheduler samples for 2021 volume 3 features

---
+++ CalendarTypes/ViewModel/ViewModel.cs
@@ -0,0 +1,161 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CalendarTypes
+{
+    public class ViewModel : NotificationObject
+    {
+        public ViewModel()
+        {
+            Events = GenerateRandomAppointments();
+            GetCalendarTypes();
+            DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+
+        private ScheduleAppointmentCollection events;
+
+        public ObservableCollection<string> CalendarTypes { get; set; }
+
+        public DateTime DisplayDate { get; set; }
+
+        public ScheduleAppointmentCollection Events
+        {
+            get { return events; }
+            set
+            {
+                events = value;
+                RaisePropertyChanged("Events");
+            }
+        }
+
+        private ScheduleAppointmentCollection GenerateRandomAppointments()
+        {
+            var WorkWeekDays = new ObservableCollection<DateTime>();
+            var WorkWeekSubjects = new ObservableCollection<string>()
+                                                           { "GoToMeeting", "Business Meeting", "Conference", "Project Status Discussion",
+                                                             "Auditing", "Client Meeting", "Generate Report", "Target Meeting", "General Meeting" };
+
+            var NonWorkingDays = new ObservableCollection<DateTime>();
+            var YearlyOccurranceSubjects = new ObservableCollection<string>() { "Wedding Anniversary", "Sam's Birthday", "Jenny's Birthday" };
+            var MonthlyOccurranceSubjects = new ObservableCollection<string>() { "Pay House Rent", "Car Service", "Medical Check Up" };
+            var WeekEndOccurranceSubjects = new ObservableCollection<string>() { "FootBall Match", "TV Show" };
+
+            var brush = new ObservableCollection<SolidColorBrush>();
+            brush.Add(new SolidColorBrush(Color.FromRgb(133, 81, 242)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(140, 245, 219)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(83, 99, 250)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(255, 222, 133)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(45, 153, 255)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(253, 183, 165)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(198, 237, 115)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(253, 185, 222)));
+            brush.Add(new SolidColorBrush(Color.FromRgb(255, 222, 133)));
+
+            Random ran = new Random();
+            DateTime today = DateTime.Now;
+            if (today.Month == 12)
+            {
+                today = today.AddMonths(-1);
+            }
+            else if (today.Month == 1)
+            {
+                today = today.AddMonths(1);
+            }
+
+            DateTime startMonth = new DateTime(today.Year, today.Month - 1, 1, 0, 0, 0);
+            int day = (int)startMonth.DayOfWeek;
+            DateTime CurrentStart = startMonth.AddDays(-day);
+
+            var appointments = new ScheduleAppointmentCollection();
+            for (int i = 0; i < 90; i++)
+            {
+                if (i % 7 == 0 || i % 7 == 6)
+                {
+                    //add weekend appointments
+                    NonWorkingDays.Add(CurrentStart.AddDays(i));
+                }
+                else
+                {
+                    //Add Workweek appointment
+                    WorkWeekDays.Add(CurrentStart.AddDays(i));
+                }
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                DateTime date = WorkWeekDays[ran.Next(0, WorkWeekDays.Count)].AddHours(ran.Next(9, 17));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[i % brush.Count],
+                    Subject = WorkWeekSubjects[i % WorkWeekSubjects.Count]
+                });
+            }
+            int j = 0;
+            int k = 0;
+            int l = 0;
+
+            while (j < YearlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)];
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[1],
+                    Subject = YearlyOccurranceSubjects[j]
+                });
+                j++;
+            }
+            while (k < MonthlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(9, 23));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[k],
+                    Subject = MonthlyOccurranceSubjects[k]
+                });
+                k++;
+            }
+            while (l < WeekEndOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(0, 23));
+                appointments.Add(new ScheduleAppointment()
+                {
+                    StartTime = date,
+                    EndTime = date.AddHours(1),
+                    AppointmentBackground = brush[l],
+                    Subject = WeekEndOccurranceSubjects[l]
+                });
+                l++;
+            }
+
+            return appointments;
+        }
+
+        private void GetCalendarTypes()
+		{
+            CalendarTypes = new ObservableCollection<string>()
+            {
+                "GregorianCalendar",
+                "HebrewCalendar",
+                "HijriCalendar",
+                "KoreanCalendar",
+                "PersianCalendar",
+                "TaiwanCalendar",
+                "ThaiCalendar",
+                "UmAlQuraCalendar",
+            };
+		}
+    }
+}
